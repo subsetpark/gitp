875d6a1271bb64d23c4632bf39328b8a0012fd8b
Z. D. Smith <cruxdestruct@gmail.com>
2014-03-27 19:23:07 -0400

    Staging all

--- gitp.py
+++ gitp.py
@@ -13,13 +13,23 @@ def chunk(lines):
             chunk = [line]
         else:
             chunk.append(line)
+    yield chunk
 
-class EditDiffCommand(sublime_plugin.TextCommand):
-    def run(self):
-        filename = self.view.file_name()
+class EditDiffCommand(sublime_plugin.WindowCommand):
+    def crunch_diff(self, str):
+        choices = [0] + [int(char) for char in str.split() if char.isdigit()]
+        filename = self.window.active_view().file_name()
         diff = subprocess.check_output(['git', 'diff', filename]).decode('UTF-8')
-        for hunk in chunk(lines(diff)):
-            sublime.message_dialog(hunk)
+        new_diff = "\n".join("\n".join(hunk) for i, hunk in enumerate(chunk(lines(diff))) if i in choices)
+        if new_diff[-1] != "\n":
+            new_diff += "\n"
+        p = subprocess.Popen(['git', 'apply', '--cached', '--recount', '--allow-overlap'], stderr=subprocess.STDOUT, stdin=subprocess.PIPE,  universal_newlines=True)
+        p.communicate(input=new_diff)
+
+    def run(self):
+        self.window.show_input_panel('Please enter choices: ', '', self.crunch_diff, None, None)
+        self.load_diff()
+        pass
 
 class DisplayHunksCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -29,7 +39,8 @@ class DisplayHunksCommand(sublime_plugin.TextCommand):
         self.view.add_regions("hunks", pts, "hunks", "dot", sublime.HIDDEN | sublime.PERSISTENT)
 
     def load_diff(self):
-        diff_lines =  subprocess.check_output(['git', 'diff', filename]).decode('UTF-8').splitlines()
+        diff_lines =  subprocess.check_output(['git', 'diff', filename],
+                    stderr=subprocess.STDOUT).decode('UTF-8').splitlines()
         hunk_metadata = [line.split()[1] for line in diff_lines if line.startswith('@@')]
         hunk_line_nos = [int(word.split(',')[0].translate({ord(i):None for i in '-+,'})) for word in hunk_metadata]
         return diff_lines, hunk_line_nos
--- poem.md
+++ poem.md
@@ -1,22 +1,18 @@
 ﻿How badly
 Enough of this stuff, Charlie
 The way of things, your mistress,
-Defies reason;
 You happy, poor thinker, you who
-Lays up your agreeable, continuous slats,
 Your elegances;
-How gently the *small* fish
 And nibbling things of hers
 Swirl at your ankles
 As you *proceed* alongside
-The shore. Keep it only under the blanket
 Nu?
 Hello Tom this my poem
+What am I doing
 To you.
 On the outside
 And by the way, run a marathon.
 Eat a canteloupe.
-In view; know your right from your left,
 Moderate the body's energies,
 Keep the bitter and the sour in right
 Proportion. After 
@@ -24,5 +20,4 @@ Proportion. After
 all, you bathe
 You rankle me.
 Understand.
-Regularly. Poorly And the world will unfold
 As she leads me out to sea.
